c47a1dd5317bc2262b9b3fd69e1b623b5ea3b2d9
kevm3 <kevm2@yahoo.com>
2017-06-21 09:46:51 -0700

    worked on portfolio features

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+  Blog.create!(
+      title: "My Blog Post #{blog}",
+      body: 'This is body content. What where why?'
+  )
+end
+puts '10 blog posts and skills created'
+
+5.times do |num|
+    Skill.create!(
+       title: "Rails #{num}",
+       percent_utilized: 15
+    )
+end
+puts '5 skills created'
+
+9.times do |num|
+    Portfolio.create!(
+       title: "Test #{num}",
+       subtitle: 'hi there',
+       body: 'This is the body',
+       main_image: 'https://placehold.it/600x400',
+       thumb_image: 'http://placehold.it/350x200'
+    )
+end
+
+puts '9 portfolio items created'
